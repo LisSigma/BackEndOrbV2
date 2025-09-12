47927ea56f0146af5b0d88af1e73497ad7b74aab
LisSigma <kainanbullatgm@gmail.com>
2025-09-12 11:29:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Render provides $PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
