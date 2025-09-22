591b7f41d6348710aecd50813fd0693beede6f67
LisSigma <kainanbullatgm@gmail.com>
2025-09-22 16:53:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,7 @@ def get_messages():
     return jsonify(messages)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Render will set the PORT env var automatically
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
